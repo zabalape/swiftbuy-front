09731ba513dc39486c21de665c34778f7741bdd3
Gabriel Zabala <88402616+zabalape@users.noreply.github.com>
2023-08-04 11:53:59 -0300

    update changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
-  content: ["./src/**/*.{html,js,jsx}"],
-=======
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
->>>>>>> develop
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
-}
-
-=======
 };
->>>>>>> develop
